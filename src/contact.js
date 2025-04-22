76239ed7ab609f44aba63ff111eb4096fc905771
Hugo Perez <hugopereztm12309@gmail.com>
2025-04-22 18:01:23 -0400

    Build contact page DOM elements

---
+++ src/contact.js
@@ -0,0 +1,28 @@
+import { createElement, createInputGroup } from "./domHelpers";
+
+export default function contactPageLoad(container) {
+
+    const title = createElement('h1', {text: 'Contact Us'});
+
+    const info = createElement('section', {id: 'info'});
+    const address = createElement('p', {text: '123 Shattered Plains'});
+    const area = createElement('p', {text: 'Planet: Roshar'});
+    const phoneNumber = createElement('p', {text: '123-456-7890'});
+    const infoEmail = createElement('p', {text: 'For all other inquiries please email kaladin@stormlightarchive.fake-email'});
+        
+    info.append(area, address, phoneNumber, infoEmail);
+
+    const formContainer = createElement('section', {id: 'form-container'});
+    const form = createElement('form', {id: 'contact-form'});
+
+    const fullNameGroup = createInputGroup('Full Name:', 'text', 'username');
+    const emailGroup = createInputGroup('Email:', 'email', 'userEmail');
+    const messageGroup = createInputGroup('Your Message:', 'text', 'userMsg')
+
+    form.append(fullNameGroup, emailGroup, messageGroup);
+    formContainer.append(form);
+
+    container.append(title, info, formContainer);
+
+    return container;
+}
