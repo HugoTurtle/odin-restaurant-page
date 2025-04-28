6e63cf7aa95de904b38996466343574f41a58682
Hugo Perez <hugopereztm12309@gmail.com>
2025-04-28 17:54:58 -0400

    Add a price and container for menu items

--- src/menu.js
+++ src/menu.js
@@ -8,44 +8,51 @@ export default function menuPageLoad(container) {
     const menu = [
         {
             name: 'Baywraps',
+            price: 10,
             description: 'rolls of thin flat bread stuffed with boiled barley and vegetables',
         },
         {
             name: 'Simberry Jam',
+            price: 4,
             description: 'according to Palates of Personality, a liking for this jam indicates a spontaneous and impulsive nature as well as a love for the outdoors.',
         },
         {
             name: 'Heartroot Tea',
+            price: 2,
             description: '',
         },
         {
             name: 'Tinkfans',
+            price: 3,
             description: '',
         },
         {
             name: 'Mudbeer',
+            price: 4,
             description: '',
         },
         {
             name: 'Pop\'ems',
+            price: 5,
             description: 'oily, fried junk food covered in powdered sugar',
         }
     ]
 
-    menu.forEach(({ name, description }) => {
-        const menuItem = createElement('li');
+    menu.forEach(({ name, price, description }) => {
+        const listItem = createElement('li')
+        const menuItemContainer = createElement('div', {class: 'menu-item-container'});
         const menuName = createElement('h3', { text: name });
-      
-        const menuDesc = createElement('p', { text: description });
+        const menuPrice = createElement('p', {text: price});
+
+        menuItemContainer.append(menuName, menuPrice);
+        listItem.append(menuItemContainer);
       
         if (description) {
-          menuDesc.classList.add('description');
-          menuItem.append(menuName, menuDesc);
-        } else {
-          menuItem.append(menuName);
+            const menuDesc = createElement('p', { text: description, class:'description'});
+            listItem.append(menuDesc);
         }
       
-        menuList.append(menuItem);
+        menuList.append(listItem);
       });
     
       menuSection.append(menuList);
